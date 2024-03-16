f71519cc1d8c125d3573a4afcc93d971b08b3cff
Vladimir <vladimir.melnik.n@gmail.com>
2024-03-16 21:19:40 +0400

    init11

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,39 +1,37 @@
-<script>
-  const nav = [
-    {
-      title: "Обо мне",
-      href: "/about",
-    },
-    {
-      title: "Портфолио",
-      href: "/portfolio",
-    },
-    {
-      title: "Контакты",
-      href: "/contact",
-    },
-  ];
-</script>
-
 <nav>
-  <div class="nav-container">
-    <a href="/" class="nav-logo" title="Back to Homepage">Дарья Любимова</a>
-    <div class="nav-links">
-      {#each nav as link}
-        <a href={link.href} class="link">{link.title}</a>
-      {/each}
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Дарья Любимова</a>
+        <div class="nav-links">
+            {#each nav as link}
+				<a href={link.href} class="link">{link.title}</a>
+            {/each}
+        </div>
     </div>
-  </div>
 </nav>
 
 <div class="container">
-  <!-- Pages will be injected below -->
-  <slot />
+    <!-- Pages will be injected below -->
+    <slot></slot>
 </div>
-
+<script>
+    	const nav = [
+		{
+			title: 'Обо мне',
+			href: '/about'
+		},
+		{
+			title: 'Портфолио',
+			href: '/portfolio'
+		},
+		{
+			title: 'Контакты',
+			href: '/contact'
+		}
+	]
+</script>
 <style>
-  .container {
-    max-width: 1400px;
-    margin: 60px auto;
-  }
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
 </style>
